7b4945943a9cee0bc1d4a66c8dbfee2f741ded2a
Anton Cheng <antonassocareer@gmail.com>
2022-10-23 15:46:01 +0800

    chore: update deploy script

--- scripts/Deploy.sol
+++ scripts/Deploy.sol
@@ -7,20 +7,28 @@ import {EthVolOracle} from "../src/EthVolOracle.sol";
 
 contract Deploy is Script {
     
-    address private constant squeethControllerMainnet =
-        address(0x64187ae08781B09368e6253F9E94951243A493D5);
+    address private constant squeethMainnet = address(0x64187ae08781B09368e6253F9E94951243A493D5);
+    address private constant squeethGoerli = address(0x50f3D0826d4E3c3d49007DBa664727B9885Dd734);
 
     function run() external {
         vm.startBroadcast();
 
-        deploy();
+        uint chainId;
+        assembly { chainId := chainid() }
+        require(chainId == 1 || chainId == 5, "wrong chain");
+
+        deploy(chainId);
 
         vm.stopBroadcast();
     }
 
     /// @dev the script currently only work with new deployer account (nonce = 0)
-    function deploy() public {
-        EthVolOracle oracle = new EthVolOracle(squeethControllerMainnet);
+    function deploy(uint256 chainId) public {
+        EthVolOracle oracle = new EthVolOracle(
+            chainId == 1 
+                ? squeethMainnet
+                : squeethGoerli
+        );
         
         uint256 gasBefore1 = gasleft();
         uint256 vol = oracle.getEthTwaIV(120);
